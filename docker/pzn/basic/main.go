6a707eb0ced0e3890aeeec0d49a849869149e842
zkh <iqbal1331167@sttpln.ac.id>
2020-11-03 17:22:22 +0700

    add docker directory

---
+++ docker/pzn/basic/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	index := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Index Page"))
+	}
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/index/", index)
+
+	http.HandleFunc("/about/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("About Page"))
+	})
+
+	// run the server
+	fmt.Println("Server running at localhost:9000")
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
