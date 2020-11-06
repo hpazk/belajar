316dee4b656403824f10bca2e9072b92da18445b
hpazk <hpazk.zkh@gmail.com>
2020-11-06 23:17:09 +0700

    course: dicoding - Front-End Web Developer Path

---
+++ courses/dicoding/front-end-developer-path/fundamental-front-end-development/ClubFinder/club.js
@@ -0,0 +1,25 @@
+const clubs = [{
+        name: "Arsenal",
+        stadium: "Emirates Stadium",
+        fanArt: "....",
+        description: "...."
+    },
+    {
+        name: "Aston Villa",
+        stadium: "Villa Park",
+        fanArt: "....",
+        description: "...."
+    },
+    {
+        name: "Bournemouth",
+        stadium: "Dean Court",
+        fanArt: "....",
+        description: "....",
+    },
+    {
+        name: "Brighton",
+        stadium: "Falmer Stadium",
+        fanArt: "....",
+        description: "....",
+    }
+];
